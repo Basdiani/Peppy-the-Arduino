754feb25addf4f25e2810beb50bb184556741056
Flocke28 <flocke@online.de>
2024-02-16 16:40:50 +0100

    Update Code.h

--- Code.h
+++ Code.h
@@ -77,6 +77,7 @@ void setup() {
   lastStartTime = millis(); // Startzeit setzen
 
   digitalWrite(ledPin, LOW);
+  delay(200);
   
   pinreset(); // Standardwerte der Outputs setzen, player stoppen
   

3f82090027aad4d8b542d335bcb184bedc3c4d43
Flocke28 <flocke@online.de>
2024-02-16 16:07:02 +0100

    Update Code.h

--- Code.h
+++ Code.h
@@ -233,6 +233,8 @@ if (Serial.available() > 0) { // Prüfen, ob Daten in der seriellen Konsole verf
         Serial.println("Unbekannter Befehl!");
         break;
     }
+  
+  wdt_reset();  /* Reset the watchdog */
 }
 
 // Überprüfen, ob der "Attract Mode" gestartet werden soll

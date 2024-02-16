03644af1d1ab8fb431497ab6697d3f1036903ebb
Flocke28 <flocke@online.de>
2024-02-16 18:52:29 +0100

    Update Code.h

--- Code.h
+++ Code.h
@@ -188,7 +188,7 @@ void loop() {
   if (programRunning) { 
 
     //  Controlls aufrufen und Überprüfen, ob das Spielende erreicht wurde
-    if controlOutputs() && (currentTime - startTime >= 60000) {
+    if (controlOutputs() && (currentTime - startTime >= 60000) {
       endGame();
     }
   }
